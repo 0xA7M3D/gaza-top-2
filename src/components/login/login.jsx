a66501fb825a6e46aab5a9b0355e745ebdc25df6
0xA7M3D <ahmed.dev.prog@gmail.com>
2026-05-24 11:47:14 +0300

    Update page login & register UI

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -5,7 +5,8 @@ import { Link, useNavigate } from "react-router-dom";
 
 
 function Login(){
-    
+    console.log("Normal text %cHello text %cGreen text", "color: red", "color: green");
+
     const [show , SetShow] = useState(false);
     const [faucus , SetFaucus] = useState({facus1:false , facus2:false});
     // console.log(faucus.facus1 , faucus.facus2);
@@ -59,7 +60,7 @@ function Login(){
     return(
         <div className="relative">
 
-            <div className={`notefication2 border-b  absolute ${close? "-top-40":"-top-3"}  ${msg.error!=null? "-top-3": "-top-20"} ${msg.error == false ? "border-green-400" :"border-red-300"} transition-all rounded-lg text-neutral-800 -translate-1/2 left-1/2 z-40 w-[350px] bg-white shadow-lg shadow-neutral-300/20`}>
+            <div className={`notefication2 border-b  absolute ${close? "-top-40":"-top-3"}  ${msg.error!=null? "-top-3": "-top-20"} ${msg.error == false ? "border-green-400" :"border-red-300"} transition-all rounded-lg text-neutral-800 -translate-1/2 left-1/2 z-40 w-[450px] bg-gray-800 shadow-lg shadow-neutral-300/20`}>
                 <div className="flex justify-between p-3 py-4 items-center">
                     <div className="ll flex items-center gap-4">
                         {
@@ -79,27 +80,27 @@ function Login(){
             </div>
 
 
-            <div className="login gap-4 shadow-2xl flex flex-col items-center relative  p-5 pt-7 bg-white/80 text-black rounded-lg w-[350px] border-0 border-violet-400">
+            <div className="login gap-4 shadow-2xl flex flex-col items-center relative  p-5  py-9 bg-gray-800/80 text-black rounded-lg w-[450px] border-0 border-violet-400">
          
                 
                 <div className="title_page absolute -translate-1/2 top-0 right-0 scale-3d -skew-x-12 bg-violet-400 p-1 px-3 text-white rounded-sm ">
                     <p>Login</p>
                 </div>
                 <div className="text_title text-3xl leading-4 w-full">
-                    <p className="text-sm text-neutral-900/90  ">Welcome to</p>
+                    <p className="text-sm text-gray-600/90  ">Welcome to</p>
                     <h3 className="text-center text-violet-500 font-bold">Gaza Top</h3>
                 </div>
                 <form onSubmit={send} method="post" className="contents">
                     <div className="inps flex flex-col py-4 w-full gap-4">
                         <div className="inp-1 flex relative items-center flex-row">
-                            <i className={`fa fa-at ${faucus.facus1 ? "text-violet-400":"text-neutral-600"}`}></i>
-                            <span className={`absolute -translate-1/2 ${faucus.facus1 ? "top-0 left-15 text-neutral-600 text-sm":"top-1/2 left-15"} transition-all  text-neutral-400 z-[1] `}> Name @ </span>
-                            <input onChange={(e)=>Set_data_user((provid)=>({...provid,user:e.target.value}))} onFocus={()=>SetFaucus((prev)=>({...prev,facus1:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus1: e.target.value?true:false}))} className={`w-full p-1 px-3 border-b ${faucus.facus1 ? "border-violet-400":"border-neutral-400"} outline-none relative z-[3]`} type="text" />
+                            <i className={`fa fa-at ${faucus.facus1 ? "text-violet-400":"text-gray-500"}`}></i>
+                            <span className={`absolute -translate-1/2 ${faucus.facus1 ? "top-0 left-15 text-gray-700 text-sm":"top-1/2 left-16"} transition-all  text-gray-600 z-[1] `}> Name @ </span>
+                            <input onChange={(e)=>Set_data_user((provid)=>({...provid,user:e.target.value}))} onFocus={()=>SetFaucus((prev)=>({...prev,facus1:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus1: e.target.value?true:false}))} className={`w-full p-1 px-3 border-b ${faucus.facus1 ? "border-violet-500/40":"border-white/10"} outline-none relative z-[3] text-gray-300 pl-5`} type="text" />
                         </div>
                         <div className="inp-1 flex relative items-center flex-row">
-                            <i className={`fa fa-key ${faucus.facus2 ? "text-violet-400":"text-neutral-600"}`}></i>
-                            <span className={`absolute -translate-1/2 ${faucus.facus2 ? "top-0 left-14 text-neutral-600 text-sm":"top-1/2 left-14"} transition-all  text-neutral-400 z-[1] `}> Password </span>
-                            <input onChange={(e)=>Set_data_user((provid)=>({...provid,pass:e.target.value}))} onFocus={()=>SetFaucus( (prev)=>({...prev, facus2:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus2:e.target.value ? true:false}))} className={`w-full p-1 px-3 border-b ${faucus.facus2 ? "border-violet-400":"border-neutral-400"} outline-none relative z-[3]`} type={`${show?"text":"password"}`} />
+                            <i className={`fa fa-key ${faucus.facus2 ? "text-violet-400":"text-gray-500"}`}></i>
+                            <span className={`absolute -translate-1/2 ${faucus.facus2 ? "top-0 left-16 text-gray-700 text-sm":"top-1/2 left-16"} transition-all  text-gray-600 z-[1] `}> Password </span>
+                            <input onChange={(e)=>Set_data_user((provid)=>({...provid,pass:e.target.value}))} onFocus={()=>SetFaucus( (prev)=>({...prev, facus2:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus2:e.target.value ? true:false}))} className={`w-full p-1 px-3 border-b ${faucus.facus2 ? "border-violet-500/40":"border-white/10"} outline-none relative z-[3] text-gray-300 pl-5`} type={`${show?"text":"password"}`} />
                             <i onClick={()=>{SetShow(!show)}} className={`far fa-eye${show ? "-slash":""} absolute cursor-pointer -translate-1/2 top-1/2 right-0 z-[4]`}></i>
                         
                         </div>
--- src/components/login/signup.jsx
+++ src/components/login/signup.jsx
@@ -23,9 +23,13 @@ function SignUp(){
     function send(){
         // console.log(data_user);
         
-            fetch("http://localhost/Gaza-Top/Gaza-Top/Api/signup.php",{
+            fetch("http://localhost:5000/signup",{
                 body: JSON.stringify(data_user),
                 method:"POST",
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
             })
             .then(resp => resp.json())
             .then(data =>{
@@ -65,36 +69,40 @@ function SignUp(){
                 </div>
             </div>
 
-            <div className="login gap-4 shadow-2xl flex flex-col items-center relative  p-5 pt-7 bg-white/80 text-black rounded-lg w-[350px] border-0 border-violet-400">
+             <div className="sign gap-4 shadow-2xl flex flex-col items-center relative  p-5  py-9 bg-gray-800/80 text-black rounded-lg w-[450px] border-0 border-violet-400">
+         
+                
                 <div className="title_page absolute -translate-1/2 top-0 right-0 scale-3d -skew-x-12 bg-violet-400 p-1 px-3 text-white rounded-sm ">
-                    <p>Sign Up</p>
+                    <p>Sign up</p>
                 </div>
                 <div className="text_title text-3xl leading-4 w-full">
-                    <p className="text-sm text-neutral-900/90  ">Welcome to</p>
+                    <p className="text-sm text-gray-600/90  ">Welcome to</p>
                     <h3 className="text-center text-violet-500 font-bold">Gaza Top</h3>
                 </div>
-                <div className="inps flex flex-col py-4 w-full gap-4">
-                    <div className="inp-1 flex relative items-center flex-row">
-                        <i className={`fa fa-user ${faucus.facus1 ? "text-violet-400":"text-neutral-600"}`}></i>
-                        <span className={`absolute -translate-1/2 ${faucus.facus1 ? "top-0 left-15 text-neutral-600 text-sm":"top-1/2 left-15"} transition-all  text-neutral-400 z-[1] `}> User Name </span>
-                        <input onChange={(e)=>Set_data_user((provid)=>({...provid,name:e.target.value}))} onFocus={()=>SetFaucus((prev)=>({...prev,facus1:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus1: e.target.value?true:false}))} className={`w-full  p-1 px-3 border-b ${faucus.facus1 ? "border-violet-400":"border-neutral-400"} outline-none relative z-[4]`} type="text" />
+                <form onSubmit={send} method="post" className="contents">
+                    <div className="inps flex flex-col py-4 w-full gap-4">
+                        <div className="inp-1 flex relative items-center flex-row">
+                            <i className={`fa fa-user ${faucus.facus1 ? "text-violet-400":"text-gray-500"}`}></i>
+                            <span className={`absolute -translate-1/2 ${faucus.facus1 ? "top-0 left-15 text-gray-700 text-sm":"top-1/2 left-14"} transition-all  text-gray-600 z-[1] `}> Name </span>
+                            <input onChange={(e)=>Set_data_user((provid)=>({...provid,user:e.target.value}))} onFocus={()=>SetFaucus((prev)=>({...prev,facus1:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus1: e.target.value?true:false}))} className={`w-full p-1 px-3 border-b ${faucus.facus1 ? "border-violet-500/40":"border-white/10"} outline-none relative z-[3] text-gray-300 pl-5`} type="text" />
+                        </div>
+                        <div className="inp-1 flex relative items-center flex-row">
+                            <i className={`fa fa-at ${faucus.facus2 ? "text-violet-400":"text-gray-500"}`}></i>
+                            <span className={`absolute -translate-1/2 ${faucus.facus2 ? "top-0 left-15 text-gray-700 text-sm":"top-1/2 left-15"} transition-all  text-gray-600 z-[1] `}> User @ </span>
+                            <input onChange={(e)=>Set_data_user((provid)=>({...provid,user:e.target.value}))} onFocus={()=>SetFaucus((prev)=>({...prev,facus2:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus2: e.target.value?true:false}))} className={`w-full p-1 px-3 border-b ${faucus.facus2 ? "border-violet-500/40":"border-white/10"} outline-none relative z-[3] text-gray-300 pl-5`} type="text" />
+                        </div>
+                        <div className="inp-1 flex relative items-center flex-row">
+                            <i className={`fa fa-key ${faucus.facus3 ? "text-violet-400":"text-gray-500"}`}></i>
+                            <span className={`absolute -translate-1/2 ${faucus.facus3 ? "top-0 left-16 text-gray-700 text-sm":"top-1/2 left-17"} transition-all  text-gray-600 z-[1] `}> Password </span>
+                            <input onChange={(e)=>Set_data_user((provid)=>({...provid,pass:e.target.value}))} onFocus={()=>SetFaucus( (prev)=>({...prev, facus3:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus3:e.target.value ? true:false}))} className={`w-full p-1 px-3 border-b ${faucus.facus3 ? "border-violet-500/40":"border-white/10"} outline-none relative z-[3] text-gray-300 pl-5`} type={`${show?"text":"password"}`} />
+                            <i onClick={()=>{SetShow(!show)}} className={`far fa-eye${show ? "-slash":""} absolute cursor-pointer -translate-1/2 top-1/2 right-0 z-[4]`}></i>
+                        
+                        </div>
                     </div>
-                    <div className="inp-1 flex relative items-center flex-row">
-                        <i className={`fa fa-at ${faucus.facus3 ? "text-violet-400":"text-neutral-600"}`}></i>
-                        <span className={`absolute -translate-1/2 ${faucus.facus3 ? "top-0 left-24 text-neutral-600 text-sm":"top-1/2 left-25"} transition-all  text-neutral-400 z-[1] whitespace-nowrap`}> Name 4 Ex:(@ahmed) </span>
-                        <input onChange={(e)=>Set_data_user((provid)=>({...provid,user:e.target.value}))} onFocus={()=>SetFaucus((prev)=>({...prev,facus3:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus3: e.target.value?true:false}))} className={`w-full  p-1 px-3 border-b ${faucus.facus3 ? "border-violet-400":"border-neutral-400"} outline-none relative z-[4]`} type="text" />
+                    <div className="btn w-full">
+                        <button className="p-1 w-full rounded cursor-pointer text-white  bg-violet-500/90 hover:bg-violet-500 hover:scale-[0.98] active:bg-violet-500 active:scale-[0.98] transition " type="submit">Sign</button>
                     </div>
-                    <div className="inp-1 flex relative items-center flex-row">
-                        <i className={`fa fa-key ${faucus.facus2 ? "text-violet-400":"text-neutral-600"}`}></i>
-                        <span className={`absolute -translate-1/2 ${faucus.facus2 ? "top-0 left-14 text-neutral-600 text-sm":"top-1/2 left-14"} transition-all  text-neutral-400 z-[1] `}> Password </span>
-                        <input onChange={(e)=>Set_data_user((provid)=>({...provid,pass:e.target.value}))} onFocus={()=>SetFaucus( (prev)=>({...prev, facus2:true}))} onBlur={(e)=> SetFaucus((prev)=> ({...prev,facus2:e.target.value ? true:false}))} className={`w-full p-1 px-3 border-b ${faucus.facus2 ? "border-violet-400":"border-neutral-400"} outline-none relative z-[4]`} type={`${show?"text":"password"}`} />
-                        <i onClick={()=>{SetShow(!show)}} className={`far fa-eye${show ? "-slash":""} absolute cursor-pointer -translate-1/2 top-1/2 right-0 z-[5]`}></i>
-                    
-                    </div>
-                </div>
-                <div className="btn w-full">
-                    <button onClick={send} className="p-1 w-full rounded cursor-pointer text-white  bg-violet-500/90 hover:bg-violet-500 hover:scale-[0.98] active:bg-violet-500 active:scale-[0.98] transition " type="submit">Sign Up</button>
-                </div>
+                </form>
                 {/* <hr className="w-1/2 text-violet-400"/> */}
                 <div className="or flex items-center w-full text-neutral-900/10">
                     <hr className="w-full" />
