06391ffd7d518b9d10dd60fd275b1cd3e3c2f539
0xA7M3D <ahmed.dev.prog@gmail.com>
2026-05-24 11:46:07 +0300

    some edite in api

--- api/index.js
+++ api/index.js
@@ -1,31 +1,48 @@
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
-
+const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host:"localhost",
     user:"root",
     password:'',
-    database:"gazatop"
+    database:"gaza_top"
+})
+
+
+connection.connect((err)=>{
+    if(err){
+        console.log("\x1b[31m%s\x1b[0m","Error Connect Db sql ):\n", err);
+        return;
+    }
+    console.log("\x1b[32m%s\x1b[0m","\n-- Connected With db sql done :)");
 })
 
 const app = express();
 
+app.use(express.urlencoded());
 app.use(cors());
 app.use(express.json());
 
-let users= [
-    {id:1,name:"ahmed"},
-    {id:2,name:"ahmed"},
-    {id:3,name:"ahmed"},
-    {id:4,name:"ahmed"},
-]
+
+app.get("/",(req,res)=>{
+    res.send("This Api For Gaza-Top");
+})
+
+app.post('/signup',(req,res)=>{
+    const {user,name,pass} = req.body;
+    res.json({
+        user:user,
+        name:name,
+        pass:pass,
+    })
+})
+
 
 let data = []
 
-app.post("/api/creat_post", async (req,res)=>{
+app.post("/api/creat_post",  (req,res)=>{
     const id = 0;
-    const title = await req.body;
+    const title = req.body;
     console.log(title);
 
     connection.query(`INSERT INTO users (id,name,pass,number,activ,activation) VALUES (?,?,?,?,?,?)`,id,title.title,'','','','')
@@ -44,6 +61,5 @@ app.get("/api/posts",(req,res)=>{
 
 
 app.listen(5000,()=>{
-    console.log("Connected.");
-    
+    console.log("\x1b[32m%s\x1b[0m","Lisetining on Port 5000");
 })
